2da4d4dbdff470a56f0b7d856a41c876484b14f5
Nihar277 <niharbharatia614@gmail.com>
2024-07-21 15:27:45 +0530

    Inquire model done

--- src/models/inqury.models.js
+++ src/models/inqury.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import { User } from "./user.models";
+import { Event } from "./event.models";
 
 const InqurySchema=new Schema({
     event_name:{
@@ -6,7 +8,32 @@ const InqurySchema=new Schema({
         ref: 'Event',
         required: true
     },
-   event_date: {}
+   event_date: {},
+   even_type:{},
+   budget:{
+    type:Number,
+    required:true
+   },
+   date:{
+    type:Date,
+   },
+   city:{
+    type:String,
+   },
+   select_artis:{
+    type:Schema.Types.ObjectId,
+    ref:'User',
+    required: true
+   },
+   email:{
+    type:String,
+   },
+   cus_name:{
+    type:String
+   },
+   contact:{
+    type:Number
+   }
 })
 
 export const Inqury= mongoose.model("Inqury",InqurySchema)
